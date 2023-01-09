fa38ab8542d195e93a4d170e75ef54fdd5c5a381
boomblocks <logansbramlett@gmail.com>
2023-01-09 22:01:06 +0000

    Saved progress on Average 2023-01-09 22:01:06 +0000

--- Average/src/main/java/Average.java
+++ Average/src/main/java/Average.java
@@ -18,6 +18,13 @@ public class Average {
      * @return the average of all the values of nums.
      */
     public double getAvg(int[] nums){
-        return 0;
+        double result = 0;
+        int i = 0;
+
+        for (int x : nums){
+            result+=x;
+            i++;
+        }
+        return result/i;
     }
 }
