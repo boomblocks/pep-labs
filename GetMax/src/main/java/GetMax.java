4e943d4fb8f58dca130c4ecb643721976f777732
boomblocks <logansbramlett@gmail.com>
2023-01-09 22:05:04 +0000

    Saved progress on GetMax 2023-01-09 22:05:04 +0000

--- GetMax/src/main/java/GetMax.java
+++ GetMax/src/main/java/GetMax.java
@@ -7,6 +7,13 @@ public class GetMax {
      * @return the largest value in arr.
      */
     public int max(int[] arr){
-        return -1;
+        int highest = Integer.MIN_VALUE;
+
+        for (int x : arr){
+            if (x > highest){
+                highest = x;
+            }
+        }
+        return highest;
     }
 }
